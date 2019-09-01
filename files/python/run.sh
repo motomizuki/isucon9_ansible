71fd11b10df77f47cadffaa990ddfd0960194b2c
hmizumoto <Hiroki.Mizumoto@sony.com>
2019-09-01 15:28:31 +0900

    add nginx and gunicorn conf

--- files/python/run.sh
+++
@@ -1,9 +0,0 @@
-export ISUBATA_DB_HOST=isucon.trial.ubuntu002
-export ISUBATA_DB_USER=isucon
-export ISUBATA_DB_PASSWORD=isucon
-
-# note: Port 5000 and 5001 are used by datadog.
-# activate trace
-ddtrace-run gunicorn --workers=10 -b '0.0.0.0:8080' app:app
-# inactivate trace
-# gunicorn --workers=10 -b '0.0.0.0:8080' app:app
